d8ce3f84b1f47038894c8ae1d6180d8d148d5070
Mithrandir <diguerra523@gmail.com>
2025-11-18 18:50:51 +0200

    fix(MD-9):clear App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import './App.css';
-import { WelcomeSlider } from './components/organisms/Sliders/WelcomeSlider/WelcomeSlider';
 
 
 function App() {
-    return <WelcomeSlider />
+
 }
 
 export default App;
