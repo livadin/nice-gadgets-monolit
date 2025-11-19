066bb939835e5db22e7ee841dda7d95e9c6abe73
Danylo <lliivadin@gmail.com>
2025-11-19 11:33:48 +0200

    WIP(MD-14): Save Work in Progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,7 @@
 import './App.css';
-import { Footer } from './components/organisms/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <div>q</div>
-      <Footer />
-    </div>
-  );
+
 }
 
 export default App;
