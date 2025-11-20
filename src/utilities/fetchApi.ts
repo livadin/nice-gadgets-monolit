869682fca6076040bef12ab73d4a9e27ebe02e8b
Mithrandir <diguerra523@gmail.com>
2025-11-21 00:42:01 +0200

    feat(MD-27):created fetch api, installed axios

---
+++ src/utilities/fetchApi.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import type { CategoryProduct, SimpleProduct } from '../types/CategoryProduct';
+
+const BASE_URL =
+  'https://fs-aug25-monolit-digital.github.io/nice-gadgets-monolit/gadgets/';
+
+function wait(delay: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
+
+function request<T>(fileName: string): Promise<T> {
+  return wait(100)
+    .then(() => axios.get(BASE_URL + fileName))
+    .then((response) => {
+      return response.data;
+    })
+    .catch(() => {
+      throw new Error();
+    });
+}
+
+export const getPhones = () => {
+    return request<CategoryProduct[]>('phones.json');
+}
+export const getTablets = () => {
+  return request<CategoryProduct[]>('tablets.json');
+};
+export const getAccessories = () => {
+  return request<CategoryProduct[]>('accessories.json');
+};
+export const getProducts = () => {
+  return request<SimpleProduct[]>('products.json');
+};
