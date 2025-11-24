5cceff903ff5dc3163d82026d7d97b2b28cbf45d
Anastasia Kravchuk <ahinata1717@gmail.com>
2025-11-24 16:48:09 +0200

    feat(MD-47): add ScrollToTop and update MainLayout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../organisms/Header';
 import { Footer } from '../organisms/Footer';
+import { ScrollToTop } from '../../utilities/ScrollToTop';
 
 export const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-white transition-colors duration-300">
       <Header />
-
+      <ScrollToTop />
       <main
         className="
         flex-1
